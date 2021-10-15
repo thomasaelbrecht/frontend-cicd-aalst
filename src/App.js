1432e24b4324ebe8684ebbc8c7eade8ba18b0add
Karine <karine.samyn@hogent.be>
2021-10-15 20:56:05 +0200

    use React context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import {useState, useMemo, useCallback} from 'react';
+import {useState, useMemo, useCallback, createContext} from 'react';
 import {PLACE_DATA} from './mock-data';
 import Transactions from './components/Transaction';
 import Places from './components/Places';
 import AddTransactionForm from './components/AddTransactionForm';
 import {useFetch} from './hooks/useFetch';
+
+export const TransactionContext = createContext();
 
 function App() {
   const [places, setPlaces] = useState(PLACE_DATA);
@@ -41,31 +43,33 @@ function App() {
 
   const filteredTransactions = useMemo(() => {
     return transactions.filter((t) => {
-      console.log('filtering...');
       return t.place.name.toLowerCase().includes(search.toLowerCase());
     });
   }, [transactions, search]);
 
   if (loading) return <h1>Loading...</h1>;
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
   return (
-    <div className='App'>
-      <AddTransactionForm places={places} onSaveTransaction={createTransaction} />
-      <div className='m-5 flex'>
-        <input
-          type='text'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className='flex-1'
-          placeholder='search'
-        />
-        <button type='button' onClick={() => setSearch(text)}>
-          Search
-        </button>
+    <TransactionContext.Provider value={{transactions:filteredTransactions}}>
+      <div className="App">
+        <AddTransactionForm places={places} onSaveTransaction={createTransaction} />
+        <div className="m-5 flex">
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="flex-1"
+            placeholder="search"
+          />
+          <button type="button" onClick={() => setSearch(text)}>
+            Search
+          </button>
+        </div>
+        <Transactions/>
+        <Places places={places} onRate={ratePlace} />
       </div>
-      <Transactions transactions={filteredTransactions}/>
-      <Places places={places} onRate={ratePlace} />
-    </div>
+    </TransactionContext.Provider>
   );
 }
 
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {IoTrashOutline, IoPencil} from 'react-icons/io5';
+import { TransactionContext } from '../App';
 
 function Transaction({date, amount, user, place}) {
   return (
     <tr>
       <td className="border w-1/4 px-4 py-2">{new Date(date).toLocaleDateString()}</td>
-      <td className="border w-1/4 px-4 py-2">
-        {user.name}
-      </td>
+      <td className="border w-1/4 px-4 py-2">{user.name}</td>
       <td className="border w-1/4 px-4 py-2">{place.name}</td>
       <td className="border w-1/4 px-4 py-2">{amount} &euro;</td>
       <td className="border w-1/4 px-4 py-2">
@@ -26,7 +25,9 @@ function Transaction({date, amount, user, place}) {
 
 const MemoizedTransaction = React.memo(Transaction);
 
-export default function Transactions({transactions}) {
+export default function Transactions() {
+  const { transactions } = useContext(TransactionContext);
+
   return (
     <table className="table-fixed m-auto">
       <tbody>
