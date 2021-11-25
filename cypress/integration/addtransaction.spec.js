0d303560b7988288590129a18b02f41f972c12fa
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-25 12:26:03 +0100

    Fix add transaction tests

--- cypress/integration/addtransaction.spec.js
+++ cypress/integration/addtransaction.spec.js
@@ -1,11 +1,14 @@
 describe("add transaction form", () => {
+  beforeEach(() => {
+    cy.login('pieter.vanderhelst@hogent.be', '12345678');
+  });
+
   it("add transaction ", () => {
     cy.visit("http://localhost:3000/transactions/add");
 
-    cy.get("[data-cy=user_input").type("Pieter");
     cy.get("[data-cy=date_input").type("2021-11-01");
     cy.get("[data-cy=place_input").select("Irish Pub");
-    cy.get("[data-cy=amount_input").type("200");
+    cy.get("[data-cy=amount_input").type("{backspace}200");
     cy.get("[data-cy=submit_transaction").click();
 
     cy.get("[data-cy=transaction_user]").eq(9).contains("Pieter");
@@ -26,13 +29,13 @@ describe("add transaction form", () => {
   it("foutieve naam", () => {
     cy.visit("http://localhost:3000/transactions/add");
 
-    cy.get("[data-cy=user_input").type("x");
+    cy.get("[data-cy=amount_input").type("{backspace}-1");
     cy.get("[data-cy=submit_transaction").click();
 
     cy.get("[data-cy=labelinput-error").should("be.visible");
-    cy.get("[data-cy=labelinput-error").eq(0).contains("Min length is 2");
+    cy.get("[data-cy=labelinput-error").eq(0).contains("min 1");
     cy.get("[data-cy=labelinput-error")
       .eq(0)
-      .should("contain", "Min length is 2");
+      .should("contain", "min 1");
   });
 });
