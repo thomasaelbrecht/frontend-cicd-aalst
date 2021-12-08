e3825318d8cf2258295f6c7c6037e8b3ff35ed0d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-12-08 13:08:47 +0100

    Refactor for CI/CD

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axiosRoot from 'axios';
 import config from '../config.json';
 
 export const axios = axiosRoot.create({
-  baseURL: config.base_url,
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL || config.base_url,
   headers: {
     // Make sure an existing token is set before the first request is executed
     Authorization: `Bearer ${localStorage.getItem(config.token_key)}`,
